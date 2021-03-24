8a84b36b07b99a91624021d40b29b57093e21bb2
DefiantBurger <37152232+DefiantBurger@users.noreply.github.com>
2021-03-24 18:05:50 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,130 @@
-bright = 0
-y = 0
+def on_button_pressed_a():
+    global x
+    if x >= 1:
+        x += -1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global x
+    if x <= 3:
+        x += 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def start():
+    basic.show_leds("""
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # # . .
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # # # .
+        # # # . .
+        # # . . .
+        # . . . .
+        . . . . .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # # # #
+        # # # # .
+        # # # . .
+        # # . . .
+        # . . . .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # .
+        # # # . .
+        # # . . .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # .
+        # # # . .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # .
+        """)
+    basic.pause(50)
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    basic.pause(100)
+    for index in range(2):
+        basic.clear_screen()
+        basic.pause(150)
+        basic.show_leds("""
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            """)
+        basic.pause(150)
+    basic.clear_screen()
+move = 0
+x = 0
+basic.pause(500)
+start()
+basic.pause(100)
+x = 2
+y = 4
+stlight = game.create_sprite(2, 4)
+basic.pause(1000)
+enemy1 = game.create_sprite(2, 0)
 
 def on_forever():
-    global bright, y
-    led.plot_brightness(0, y, bright)
-    bright += 25
-    y += 1
-    if y > 4:
-        y = 0
-    if bright > 225:
-        bright = 0
+    global move
+    move += 1
+    if move == 20:
+        move = 0
+        enemy1.change(LedSpriteProperty.Y, 1)
+    stlight.set(LedSpriteProperty.X, x)
+    stlight.set(LedSpriteProperty.Y, y)
+    if enemy1.get(LedSpriteProperty.X) == stlight.get(LedSpriteProperty.X) and enemy1.get(LedSpriteProperty.Y) == stlight.get(LedSpriteProperty.Y):
+        game.pause()
+        basic.show_leds("""
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            """)
+    if enemy1.get(LedSpriteProperty.Y) == 4:
+        basic.pause(500)
+        enemy1.set(LedSpriteProperty.Y, 0)
+        enemy1.set(LedSpriteProperty.X, randint(0, 4))
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -95,6 +95,7 @@ function start () {
     }
     basic.clearScreen()
 }
+let move = 0
 let x = 0
 basic.pause(500)
 start()
@@ -104,10 +105,17 @@ let y = 4
 let stlight = game.createSprite(2, 4)
 basic.pause(1000)
 let enemy1 = game.createSprite(2, 0)
+let speed = 0
 basic.forever(function () {
     stlight.set(LedSpriteProperty.X, x)
     stlight.set(LedSpriteProperty.Y, y)
-    if (enemy1.get(LedSpriteProperty.Y) == stlight.get(LedSpriteProperty.Y) && enemy1.get(LedSpriteProperty.Y) == stlight.get(LedSpriteProperty.Y)) {
+    move += 1
+    if (move == 50 - speed) {
+        move = 0
+        enemy1.change(LedSpriteProperty.Y, 1)
+    }
+    if (enemy1.get(LedSpriteProperty.X) == stlight.get(LedSpriteProperty.X) && enemy1.get(LedSpriteProperty.Y) == stlight.get(LedSpriteProperty.Y)) {
+        game.pause()
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -115,10 +123,13 @@ basic.forever(function () {
             . # . # .
             # . . . #
             `)
-        game.pause()
     }
-})
-basic.forever(function () {
-    basic.pause(1000)
-    enemy1.change(LedSpriteProperty.Y, 1)
+    if (enemy1.get(LedSpriteProperty.Y) == 4) {
+        basic.pause(500)
+        enemy1.set(LedSpriteProperty.Y, 0)
+        enemy1.set(LedSpriteProperty.X, randint(0, 4))
+        if (speed <= 48) {
+            speed += 1
+        }
+    }
 })
